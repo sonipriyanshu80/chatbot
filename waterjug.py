585380eba12f146c95254b752d376d34d2fddf4c
Priyanshu soni <sonipriyanshu80@gmail.com>
2025-10-08 08:38:56 +0530

    waterjug.py

--- waterjug.py
+++ waterjug.py
@@ -1,11 +1,11 @@
 def waterjug(jug1,jug2,target,maxjug1,maxjug2,visited):
     if jug1==target or jug2==target:
-        print("target reached")
+        print("target reached", jug1, jug2)
         return True
     if (jug1,jug2) in visited:
         return False
     visited.add((jug1,jug2))
-    print("current state",jug1,jug2)
+    print("current state",(jug1,jug2))
     if waterjug(maxjug1,jug2,target,maxjug1,maxjug2,visited):
         return True
     if waterjug(jug1,maxjug2,target,maxjug1,maxjug2,visited):
@@ -28,43 +28,3 @@ target=2
 print("steps to reach the target")
 waterjug(0,0,target,maxjug1,maxjug2,set())
     
-'''
-def waterjug(jug1, jug2, target, maxjug1, maxjug2, visited):
-    if jug1 == target or jug2 == target:
-        print("Target reached")
-        return True
-    if (jug1, jug2) in visited:
-        return False
-    visited.add((jug1, jug2))
-    print("Current state:",( jug1, jug2))
-
-    # Fill Jug1
-    if waterjug(maxjug1, jug2, target, maxjug1, maxjug2, visited):
-        return True
-    # Fill Jug2
-    if waterjug(jug1, maxjug2, target, maxjug1, maxjug2, visited):
-        return True
-    # Empty Jug1
-    if waterjug(0, jug2, target, maxjug1, maxjug2, visited):
-        return True
-    # Empty Jug2
-    if waterjug(jug1, 0, target, maxjug1, maxjug2, visited):
-        return True
-    # Pour Jug1 into Jug2
-    pour1 = min(jug1, maxjug2 - jug2)
-    if waterjug(jug1 - pour1, jug2 + pour1, target, maxjug1, maxjug2, visited):
-        return True
-    # Pour jug2 into Jug1
-    #pour2 = min(maxjug1 - jug1, jug2)
-    if waterjug(jug1 + pour1, jug2 - pour1, target, maxjug1, maxjug2, visited):
-        return True
-
-    return False
-
-# Driver code
-maxjug1 = 4
-maxjug2 = 3
-target = 2
-print("Steps to reach the target:")
-waterjug(0, 0, target, maxjug1, maxjug2, set())
-'''
